3599b1ffb9abed8941d8e41f9a5613c4bad873ef
Lei Wang <lei@torpago.com>
2024-07-23 22:14:19 -0700

    add gen-api.sh

---
+++ gen-api.sh
@@ -0,0 +1,6 @@
+set -ex
+
+
+# wget "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.7.0/openapi-generator-cli-7.7.0.jar"
+
+java -jar openapi-generator-cli-7.7.0.jar generate -i https://raw.githubusercontent.com/marqeta/marqeta-openapi/main/yaml/CoreAPI.yaml  -g go -o openapi -p packageName=openapi -p structPrefix=true -p enumClassPrefix=true
