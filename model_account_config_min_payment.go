86e2d121c766a45f600fc71aa4779bca5ddbac3d
Eric Chou <eric@torpago.com>
2024-07-24 13:07:27 -0700

    fix: add decimal import

--- model_account_config_min_payment.go
+++ model_account_config_min_payment.go
@@ -14,6 +14,7 @@ package openapi
 import (
 	"encoding/json"
 	"time"
+	"github.com/shopspring/decimal"
 )
 
 // checks if the AccountConfigMinPayment type satisfies the MappedNullable interface at compile time
