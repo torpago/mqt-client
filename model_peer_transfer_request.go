40ec9766aa69d0ccb301b9497429f3f3d30da6c5
Eric Chou <eric@torpago.com>
2024-07-24 13:11:50 -0700

    fix: add decimal import

--- model_peer_transfer_request.go
+++ model_peer_transfer_request.go
@@ -15,6 +15,7 @@ import (
 	"encoding/json"
 	"bytes"
 	"fmt"
+	"github.com/shopspring/decimal"
 )
 
 // checks if the PeerTransferRequest type satisfies the MappedNullable interface at compile time
