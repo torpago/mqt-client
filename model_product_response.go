67af813e74d91ff03e9e2287976633f8bdc9462a
Eric Chou <eric@torpago.com>
2024-07-24 13:16:36 -0700

    fix: add decimal import

--- model_product_response.go
+++ model_product_response.go
@@ -14,6 +14,7 @@ package openapi
 import (
 	"encoding/json"
 	"time"
+	"github.com/shopspring/decimal"
 )
 
 // checks if the ProductResponse type satisfies the MappedNullable interface at compile time
