e08ee5b986396df57989ba464af611b3b29ce8e4
Eric Chou <eric@torpago.com>
2024-07-24 13:11:53 -0700

    fix: add decimal import

--- model_payment_schedule_create_req.go
+++ model_payment_schedule_create_req.go
@@ -15,6 +15,7 @@ import (
 	"encoding/json"
 	"bytes"
 	"fmt"
+	"github.com/shopspring/decimal"
 )
 
 // checks if the PaymentScheduleCreateReq type satisfies the MappedNullable interface at compile time
