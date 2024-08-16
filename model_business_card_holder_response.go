40601b0ebb7dbb9ae370195217eb7599bbcb0182
Eric Chou <eric@torpago.com>
2024-08-16 11:15:10 -0700

    fix: remove required for business card holder unmarshal

--- model_business_card_holder_response.go
+++ model_business_card_holder_response.go
@@ -1416,8 +1416,8 @@ func (o *BusinessCardHolderResponse) UnmarshalJSON(data []byte) (err error) {
 	// by unmarshalling the object into a generic map with string keys and checking
 	// that every required field exists as a key in the generic map.
 	requiredProperties := []string{
-		"created_time",
-		"last_modified_time",
+		// "created_time",
+		// "last_modified_time",
 	}
 
 	allProperties := make(map[string]interface{})
