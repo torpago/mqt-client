95c97080f2bc0274763f3acd16f7633f6813cf4e
Shuozeli <szsuzyli@gmail.com>
2025-10-17 14:17:28 -0700

    add service func to post commando mode api

--- api_commando_mode.go
+++ api_commando_mode.go
@@ -513,3 +513,113 @@ func (a *CommandoModeAPIService) GetCommandomodesTransitionsTokenExecute(r Comma
 
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
+
+type CommandoModeAPIPostCommandomodesRequest struct {
+	ctx context.Context
+	ApiService *CommandoModeAPIService
+	commandoModeRequest *CommandoModeRequest
+}
+
+func (r CommandoModeAPIPostCommandomodesRequest) CommandoModeRequest(commandoModeRequest CommandoModeRequest) CommandoModeAPIPostCommandomodesRequest {
+	r.commandoModeRequest = &commandoModeRequest
+	return r
+}
+
+func (r CommandoModeAPIPostCommandomodesRequest) Execute() (*CommandoModeResponse, *http.Response, error) {
+	return r.ApiService.PostCommandomodesExecute(r)
+}
+
+/*
+PostCommandomodes Create Commando Mode control set
+
+Create a Commando Mode control set.
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @return CommandoModeAPIPostCommandomodesRequest
+*/
+func (a *CommandoModeAPIService) PostCommandomodes(ctx context.Context) CommandoModeAPIPostCommandomodesRequest {
+	return CommandoModeAPIPostCommandomodesRequest{
+		ApiService: a,
+		ctx: ctx,
+	}
+}
+
+// Execute executes the request
+//  @return CommandoModeResponse
+func (a *CommandoModeAPIService) PostCommandomodesExecute(r CommandoModeAPIPostCommandomodesRequest) (*CommandoModeResponse, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodPost
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  *CommandoModeResponse
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "CommandoModeAPIService.PostCommandomodes")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/commandomodes"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.commandoModeRequest == nil {
+		return localVarReturnValue, nil, reportError("commandoModeRequest is required and must be specified")
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.commandoModeRequest
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
